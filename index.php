4b0956da60c2f78f3efca4a9c16c7d718f9c98c4
Isack Ramirez <isack.ramirez1@gmail.com>
2022-06-29 16:46:10 -0500

    sucsessfully set up curl get request with api key as header, returning raw data

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$ch = curl_init();
+
+$options = array(
+    CURLOPT_URL => 'https://api.sightmap.com/v1/assets/1273/multifamily/units',
+    CURLOPT_RETURNTRANSFER =>1,
+    CURLOPT_SSL_VERIFYHOST =>false,
+    CURLOPT_SSL_VERIFYPEER=>false,
+    CURLOPT_HTTPHEADER => array(
+        "API-Key: 7d64ca3869544c469c3e7a586921ba37"
+    )
+);
+
+curl_setopt_array($ch,$options);
+
+$response = curl_exec($ch);
+
+if($response===false){
+    echo curl_error($ch);
+}
+
+curl_close($ch);
+
+print_r($response);
+
+echo "hello world";
