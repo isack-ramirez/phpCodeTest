56791679ce8fd7569c1ef9d94f4f52a66ef8231f
Isack Ramirez <isack.ramirez1@gmail.com>
2022-06-30 17:46:03 -0500

    all done, ugly in style, could refactor to use bootstrap and make it look nicer, but i wanted to do this in the most vanilla php way possible

--- index.php
+++ index.php
@@ -53,24 +53,31 @@ $html = '
 <!DOCTYPE html>
 <html>
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="./style.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 
 
+<div class="buttonContainer">
+<a href="#list1"><button>Jump to list 1</button></a>
+<a href="#list2"><button>Jump to list 2</button></a>
+</div>
 
-<div class="grid-container">
+<div class="grid-container"id="list1">
 
   <div class="grid-item" id="grid-item1"><h6>Unit number</h6></div>
-  <div class="grid-item"id="grid-item2" ><h6>Floor ID </h6></div>
+  <div class="grid-item"id="grid-item2" ><h6>Area</h6></div>
   <div class="grid-item" id="grid-item3"><h6>Last updated</h6></div>  
 </div>
 
-<div class="grid-container">
+<div class="grid-container"id="list2">
 
   <div class="grid-item" id="grid-item4"><h6>Unit number</h6></div>
-  <div class="grid-item"id="grid-item5" ><h6>Floor ID </h6></div>
+  <div class="grid-item"id="grid-item5" ><h6>Area</h6></div>
   <div class="grid-item" id="grid-item6"><h6>Last updated</h6></div>  
 </div>
 
@@ -97,7 +104,7 @@ for($i=0;$i<$j;$i++){
   
     $descBox->appendChild($appended);
 
-    $test = $areaValue1[$i]->floor_id;
+    $test = $areaValue1[$i]->area;
     $descBox = $doc->getElementById('grid-item2');
     $appended = $doc->createElement('li', $test);
     $descBox->appendChild($appended);
@@ -145,7 +152,7 @@ for($i=0;$i<$j;$i++){
     $descBox->appendChild($appended);
 
 
-    $test = $areaValueGreaterThanOne[$i]->floor_id;
+    $test = $areaValueGreaterThanOne[$i]->area;
     $descBox = $doc->getElementById('grid-item5');
     $appended = $doc->createElement('li', $test);
     $descBox->appendChild($appended);
